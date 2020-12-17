33acdf2d921d18a44d1238865e686a56469bed44
username@email.com <hertzn@163.com>
2020-12-17 10:09:24 +0800

    增加摄像机敌人

---
+++ Assets/Scripts/Enemy/CameraEnemy/CameraEnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 摄像机敌人控制、攻击
+/// </summary>
+public class CameraEnemyController : MonoBehaviour
+{
+    public SearchTarget seaTarget;
+    public float rayTime = 1f;
+    public float attackTime = 1f;
+    public float attackCooldownTime = 5f;
+    private bool isRaying = false;
+    private bool isAttacking = false;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (seaTarget.canAttack)
+        {
+            if (timer > attackCooldownTime)
+            {
+                timer = 0;
+                //jiguang
+                Debug.Log("Ray");
+                isRaying = true;
+            }
+            else if(timer >= 1f && isRaying)
+            {
+                isRaying = false;
+                //gongji
+                Debug.Log("attack");
+                isAttacking = true;
+            }
+            else if (timer > 2f && isAttacking)
+            {
+                isAttacking = false;
+                //stop
+                Debug.Log("stop");
+            }
+            timer += Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/CameraEnemy/SearchTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 摄像机敌人搜敌
+/// </summary>
+public class SearchTarget : MonoBehaviour
+{
+    private Collider2D target;
+    public bool canAttack = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            target = other;
+            canAttack = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!target && other.tag == "Player") 
+        {
+            target = other;
+            canAttack = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other == target)
+        {
+            target = null;
+            canAttack = false;
+        }
+    }
+}
